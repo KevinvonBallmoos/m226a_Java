bc8d81d6e63a53002ca84653e825fac7673e76b1
Subscaaper <63253193+Subscaaper@users.noreply.github.com>
2020-09-19 19:15:45 +0200

    Minesweeper

--- src/main/java/edu/Minesweeper/PlayGround.java
+++ src/main/java/edu/Minesweeper/PlayGround.java
@@ -6,15 +6,18 @@ import java.util.Random;
 
 public class PlayGround {
     /**
-     * generates PlayGround with 5 - 10 bombs
+     * generates PlayGround with 10 minees on random places
      */
 
-    String bomb = "*";
-    int rowX = 9, colY = 9;
-    String[][] playGroundArray = new String[rowX][colY];
-    int countMines = 10;
+    private final int rowX = 9;
+    private final int colY = 9;
+    private final String[][] playGroundArray = new String[rowX][colY];
+    private final String mines = "*";
     Random random = new Random();
 
+    public String getMines() {
+        return mines;
+    }
 
     public String[][] getPlayGroundArray() {
         return playGroundArray;
@@ -32,15 +35,14 @@ public class PlayGround {
                 if (y == 0) {
                     playGroundArray[x][y] = String.valueOf(x - 1);
                     if (x - 1 < 0) {
-                        playGroundArray[x][y] = "+";
+                        playGroundArray[x][y] = "";
                     }
                 }
                 if (x > 0) {
                     if (y > 0) {
-                        playGroundArray[x][y] = "+";
+                        playGroundArray[x][y] = "";
                     }
                 }
-
             }
             if (x == 8) {
                 setMines();
@@ -52,21 +54,20 @@ public class PlayGround {
     public void setMines() {
 
 
-
-        for (int bombX = 1; bombX < countMines; bombX++) {
-
-            for (int bombY = 1; bombY < ; bombY += isMine + 1) {
-
-                getPlayGroundArray()[bombX][bombY] = bomb;
+        final int numberOfMines = 10;
+        for (int mineX = 1; mineX < 9; mineX++) {
+            for (int mineY = 1; mineY < 2; mineY++) {
+                getPlayGroundArray()[mineX][random.nextInt(playGroundArray.length -1) + 1] = mines;
             }
         }
-        System.out.println("The PlayGround is ready and there are " + countMines + " bombs hidden.");
+        System.out.println("The PlayGround is generating.");
+        System.out.println("...");
+        System.out.println("The PlayGround is ready and there are " + numberOfMines + " mines hidden.");
         System.out.println("");
 
        /* int countOfX = Math.floorDiv(lengthOfArray - 1, rand);
         randomXO();
         for (int x = 0; x < lengthOfArray; x += countOfX + 1) {
             XO[x] = 'X';*/
-        }
     }
 }
